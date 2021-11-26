d2b1a8c7709443ffae7c06d1a9ac1b6de8df221d
benjaminbills <benjaminoba234@gmail.com>
2021-11-26 16:13:39 +0300

    update

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
   title=models.CharField(max_length=100)
+  intro=models.CharField(max_length=100, blank=True, default='Blank')
   image=models.ImageField(upload_to='project_photos/')
   description=models.TextField()
   link=models.URLField()
+  githublink=models.URLField(blank=True, default='Blank')
