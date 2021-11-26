c04bcd4cd7672e8dd5f21688f42529614681ba00
benjaminbills <benjaminoba234@gmail.com>
2021-11-26 14:24:40 +0300

    add all

--- core/settings.py
+++ core/settings.py
@@ -44,10 +44,11 @@ INSTALLED_APPS = [
     'api',
     'cloudinary_storage',
     'cloudinary',
-    'django-cors-headers'
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
